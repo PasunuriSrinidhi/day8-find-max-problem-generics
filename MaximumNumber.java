83e1d551ee225ae2a0426adb902dd1e73d153d9d
Pasunuri Srinidhi <93040752+PasunuriSrinidhi@users.noreply.github.com>
2023-11-28 23:10:33 +0530

    Refactored the code to create Generic Class

--- MaximumNumber.java
+++ MaximumNumber.java
@@ -1,27 +1,54 @@
 import java.util.*;
-public class MaximumNumber {
-	private static <T extends Comparable> void getMaximum(T x, T y, T z) {
 
-		T max = x;
-		if (y.compareTo(x) > 0) {
-			max = y;
-		}
-		if (z.compareTo(max) > 0) {
-			max = z;
-		}
+public class MaximumNumber<T extends Comparable<T>> {
+    private T x, y, z;
 
-		System.out.println("Maximum ::" + max);
-	}
+    public MaximumNumber(T x, T y, T z) {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
 
-	public static void main(String[] args) {
-		System.out.println("Welcome to Generic Program.");
-		Integer x = 10, y = 23, z = 15;
-		Float d = 12.5f, e = 13.4f, f = 22.0f;
-		String s1 = "Apple", s2 = "Peach", s3 = "Banana";
+    private T getMaximum() {
+        T max = x;
+        if (y.compareTo(x) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        return max;
+    }
 
-		MaximumNumber max = new MaximumNumber();
-		max.getMaximum(x, y, z);
-		max.getMaximum(d, e, f);
-		max.getMaximum(s1, s2, s3);
-	}
+    public void testMaximum() {
+        T max = getMaximum();
+        System.out.println("Maximum :: " + max);
+    }
+
+    public static <T extends Comparable<T>> void testMaximum(T x, T y, T z) {
+        T max = x;
+        if (y.compareTo(x) > 0) {
+            max = y;
+        }
+        if (z.compareTo(max) > 0) {
+            max = z;
+        }
+        System.out.println("Maximum :: " + max);
+    }
+
+    public static void main(String[] args) {
+        System.out.println("Welcome to the Generic Program.");
+
+        // Integer test
+        MaximumNumber<Integer> intMax = new MaximumNumber<>(10, 23, 15);
+        intMax.testMaximum();
+
+        // Float test
+        MaximumNumber<Float> floatMax = new MaximumNumber<>(12.5f, 13.4f, 22.0f);
+        floatMax.testMaximum();
+
+        // String test
+        MaximumNumber<String> stringMax = new MaximumNumber<>("Apple", "Peach", "Banana");
+        stringMax.testMaximum();
+    }
 }
